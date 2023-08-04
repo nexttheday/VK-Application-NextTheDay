47dc3aab3a6b116457b24b2b2397404027d17317
nexttheday <141416627+nexttheday@users.noreply.github.com>
2023-08-05 03:37:53 +0700

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,12 +1,12 @@
-bridge.send('VKWebAppInit')
-  .then((data) => { 
-    if (data.result) {
-      alert("++++");
-    } else {
-      alert("error");
-    }
-  })
-  .catch((error) => {
-    // Ошибка
-    console.log(error);
-  });
+// bridge.send('VKWebAppInit')
+//   .then((data) => { 
+//     if (data.result) {
+//       alert("++++");
+//     } else {
+//       alert("error");
+//     }
+//   })
+//   .catch((error) => {
+//     // Ошибка
+//     console.log(error);
+//   });
