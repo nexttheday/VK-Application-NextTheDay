cfef9a9296130450416a44b4cfca4ef9d80ae592
nexttheday <141416627+nexttheday@users.noreply.github.com>
2023-08-05 04:09:50 +0700

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,7 @@
 vkBridge.send('VKWebAppInit'); // Иницилизация приложения на сервере Вконтакте
 
+alert("++=")
+
 function ControlInJS(cmd) {
   if (cmd == "ActiveMenuCreatePlan") { // Если выбрана кнпока "Создать тренировку"
     vkBridge.send('VKWebAppAddToFavorites');
